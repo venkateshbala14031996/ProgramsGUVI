40d4e10b9b42e65277a2e9c9259757d6440fe1d6
venkateshbala14031996 <venkateshbala1996@gmail.com>
2017-01-05 18:31:23 +0530

    Create CheckOddEven.java
    This Program is used to check whether the given Number is Even or Odd

---
+++ CheckOddEven.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+ class CheckEvenOdd {
+public static void main (String args[]) {
+int number;
+Scanner in=new Scanner(System.in);
+number = in.nextInt();
+if(number %2 == 0) {
+ System.out.println("Even");
+ }
+ else {
+   System.out.println("Odd");
+   }
+ }
+}
+
+
+ 
